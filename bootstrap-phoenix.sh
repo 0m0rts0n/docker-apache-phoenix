2ac7f99549d0951772d2239e42e61d3c57338dba
CheyenneForbes <cheyenne.osanu.forbes@gmail.com>
2017-04-25 02:46:47 -0500

    Update bootstrap-phoenix.sh

--- bootstrap-phoenix.sh
+++ bootstrap-phoenix.sh
@@ -5,32 +5,54 @@
 : ${HBASE_HOME:=/opt/hbase}
 : ${PHOENIX_HOME:=/opt/phoenix}
 
-rm /tmp/*.pid
+if [[ $1 == "-stop" ]]; then
+	$HADOOP_PREFIX/sbin/stop-dfs.sh
+	$HADOOP_PREFIX/sbin/stop-yarn.sh
+	$HBASE_HOME/bin/stop-hbase.sh
+	$PHOENIX_HOME/bin/queryserver.py stop
+else
 
-# installing libraries if any - (resource urls added comma separated to the ACP system variable)
-cd $HADOOP_PREFIX/share/hadoop/common ; for cp in ${ACP//,/ }; do  echo == $cp; curl -LO $cp ; done; cd -
+	rm /tmp/*.pid
 
-service sshd start
+	# installing libraries if any - (resource urls added comma separated to the ACP system variable)
+	cd $HADOOP_PREFIX/share/hadoop/common ; for cp in ${ACP//,/ }; do  echo == $cp; curl -LO $cp ; done; cd -
 
-export JAVA_HOME=/opt/jre
+	service sshd start
 
-$HADOOP_PREFIX/sbin/start-dfs.sh
-$HADOOP_PREFIX/sbin/start-yarn.sh
-$HBASE_HOME/bin/start-hbase.sh
+	export JAVA_HOME=/opt/jre
 
-if [[ $1 == "-d" ]]; then
-  while true; do sleep 1000; done
-fi
+	declare -a files=(
+		'/opt/hbase/conf/hbase-site.xml'
+	)
 
-if [[ $1 == "-bash" ]]; then
-  /bin/bash
-fi
+	for file in "${files[@]}"; do
+		if [ -f "${file}.bak" ]; then
+			cp "${file}.bak" "${file}"
+		else
+			cp "${file}" "${file}.bak"
+		fi
 
-if [[ $1 == "-sqlline" ]]; then
-  $PHOENIX_HOME/bin/sqlline-thin.py localhost
-fi
+		sed -i "s/HOST_NAME/${HOSTNAME}/g" "${file}"
+	done
 
-if [[ $1 == "-qs" ]]; then
-  echo "Starting queryserver"
-  $PHOENIX_HOME/bin/queryserver.py
+	$HADOOP_PREFIX/sbin/start-dfs.sh
+	$HADOOP_PREFIX/sbin/start-yarn.sh
+	$HBASE_HOME/bin/start-hbase.sh
+
+	if [[ $1 == "-d" ]]; then
+	  while true; do sleep 1000; done
+	fi
+
+	if [[ $1 == "-bash" ]]; then
+	  /bin/bash
+	fi
+
+	if [[ $1 == "-sqlline" ]]; then
+	  $PHOENIX_HOME/bin/sqlline-thin.py localhost
+	fi
+
+	if [[ $1 == "-qs" ]]; then
+	  echo "Starting queryserver"
+	  $PHOENIX_HOME/bin/queryserver.py
+	fi
 fi
