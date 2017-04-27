66cebefff36d6ef55849937315e37a0ac0bf4998
CheyenneForbes <cheyenne.osanu.forbes@gmail.com>
2017-04-27 03:43:52 -0500

    Update bootstrap-phoenix.sh

--- bootstrap-phoenix.sh
+++ bootstrap-phoenix.sh
@@ -1,42 +1,32 @@
 #!/bin/bash
 
-: ${HADOOP_PREFIX:=/opt/hadoop}
-#: ${ZOO_HOME:=/opt/zookeeper}
-: ${HBASE_HOME:=/opt/hbase}
-: ${PHOENIX_HOME:=/opt/phoenix}
+
+: ${HADOOP_PREFIX:=/usr/local/hadoop}
+: ${ZOO_HOME:=/usr/local/zookeeper}
+: ${HBASE_HOME:=/usr/local/hbase}
+: ${PHOENIX_HOME:=/usr/local/phoenix}
 
 if [[ $1 == "-stop" ]]; then
 	$HADOOP_PREFIX/sbin/stop-dfs.sh
 	$HADOOP_PREFIX/sbin/stop-yarn.sh
+	$ZOO_HOME/bin/zkServer.sh stop
 	$HBASE_HOME/bin/stop-hbase.sh
 	$PHOENIX_HOME/bin/queryserver.py stop
 else
-
 	rm /tmp/*.pid
+
+	$HADOOP_PREFIX/etc/hadoop/hadoop-env.sh
 
 	# installing libraries if any - (resource urls added comma separated to the ACP system variable)
 	cd $HADOOP_PREFIX/share/hadoop/common ; for cp in ${ACP//,/ }; do  echo == $cp; curl -LO $cp ; done; cd -
 
+	sed s/HOSTNAME/$HOSTNAME/ /usr/local/hadoop/etc/hadoop/core-site.xml.template > /usr/local/hadoop/etc/hadoop/core-site.xml
+
 	service sshd start
-
-	export JAVA_HOME=/opt/jre
-
-	declare -a files=(
-		'/opt/hbase/conf/hbase-site.xml'
-	)
-
-	for file in "${files[@]}"; do
-		if [ -f "${file}.bak" ]; then
-			cp "${file}.bak" "${file}"
-		else
-			cp "${file}" "${file}.bak"
-		fi
-
-		sed -i "s/HOST_NAME/${HOSTNAME}/g" "${file}"
-	done
 
 	$HADOOP_PREFIX/sbin/start-dfs.sh
 	$HADOOP_PREFIX/sbin/start-yarn.sh
+	$ZOO_HOME/bin/zkServer.sh start
 	$HBASE_HOME/bin/start-hbase.sh
 
 	if [[ $1 == "-d" ]]; then
