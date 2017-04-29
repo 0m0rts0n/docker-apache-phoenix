b4a6d500bcb3cea2c55c4866af1c3790197a1559
CheyenneForbes <cheyenne.osanu.forbes@gmail.com>
2017-04-28 21:19:59 -0500

    Update hbase-env.sh

--- config/hbase-env.sh
+++ config/hbase-env.sh
@@ -25,7 +25,7 @@
 # into the startup scripts (bin/hbase, etc.)
 
 # The java implementation to use.  Java 1.8+ required.
-# export JAVA_HOME=/usr/java/jdk1.8.0/
+export JAVA_HOME=/usr/java/default
 
 # Extra Java CLASSPATH elements.  Optional.
 # export HBASE_CLASSPATH=
